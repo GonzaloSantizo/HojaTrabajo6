e053a8f886ca2b67ab4db45bb895ada76c4b1bfa
GonzaloSantizo <84192252+GonzaloSantizo@users.noreply.github.com>
2022-03-14 10:10:02 -0600

    Second commit
    Added function to read .txt file and first implementation of hashmap

--- Main.java
+++ Main.java
@@ -1,20 +1,44 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 
 /**
  *
- * @author HP OMEN
+ * @author Gonzalo Santizo 21504
  */
+
+
+import java.io.*;
+import java.util.*;
+
+
 public class Main {
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.println("Hello world! :)");
+public static void main( String[] args ) throws IOException
+{
+    String filePath = "safecopy.txt"; 
+    HashMap<String, String> map = new HashMap<String, String>();
+
+    String line;
+    BufferedReader reader = new BufferedReader(new FileReader(filePath));
+    while ((line = reader.readLine()) != null)
+    {
+        String[] parts = line.split(":", 2);
+        if (parts.length >= 2)
+        {
+            String key = parts[1];
+            String value = parts[0];
+            map.put(key, value);
+        } else {
+            System.out.println("ignoring line: " + line);
+        }
     }
+
+    for (String key : map.keySet())
+    {
+        System.out.println(key + ":" + map.get(key));
+    }
+    reader.close();
+}
     
 }
