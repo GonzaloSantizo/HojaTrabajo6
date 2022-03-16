8ae38309a1fd6d2598bf4771cf72aa0f2b4682cb
GonzaloSantizo <84192252+GonzaloSantizo@users.noreply.github.com>
2022-03-16 13:44:13 -0600

    Third commit
    implemented opc 1, added hashmap structure to src code

--- Main.java
+++ Main.java
@@ -7,6 +7,7 @@
 
 import java.io.*;
 import java.util.*;
+import java.util.Scanner;
 
 
 public class Main {
@@ -16,6 +17,10 @@ public class Main {
      */
 public static void main( String[] args ) throws IOException
 {
+    
+    HashMap<String, String> MiCarritoHash = new HashMap<String, String>(); // carrito Hashmap normal
+    
+    
     String filePath = "safecopy.txt"; 
     HashMap<String, String> map = new HashMap<String, String>();
 
@@ -39,6 +44,53 @@ public static void main( String[] args ) throws IOException
         System.out.println(key + ":" + map.get(key));
     }
     reader.close();
-}
     
+        Scanner scan = new Scanner(System.in);
+        int opcion = 0;
+
+        while (opcion != 4) {
+
+            System.out.println("\n\nBienvenido al simulador de carrito de compra que estructura quieres usar?");
+            System.out.println("1. Nueva hashmap");
+            System.out.println("2. Nuevo Linked HashMap");
+            System.out.println("3. Nuevo Treemap");
+            System.out.println("4. Exit\n\n");
+
+            opcion = scan.nextInt();
+
+            if (opcion == 1) {
+                // nuevo hashmap
+                scan = new Scanner(System.in);
+                String CurrentKey;
+                String CurrentValue;
+                
+                
+                System.out.println("Que articulo quieres agregar?");
+                CurrentKey = scan.nextLine();
+                
+                
+                System.out.println("Que categoria es?");
+                CurrentValue = scan.nextLine();
+                
+                
+                MiCarritoHash.put(CurrentKey, CurrentValue);
+                
+                System.out.println("En tu carrito hay:\n\n");
+                
+                for (String MyItem: MiCarritoHash.keySet()) 
+                {
+                String key = MyItem;
+                String value = MiCarritoHash.get(MyItem);
+                System.out.println(key + " " + value);
+                }
+
+            } else if (opcion == 2) {
+                // Nuevo Linked HashMap
+            } else if (opcion == 3) {
+                // Nuevo Treemap
+            } else if (opcion == 4) {
+                // exit
+            }
+        }
+    }   
 }
